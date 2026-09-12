b32cdb6f4501cf5db8e618c79d251f10b9c2a84b
R B <41969592+crypto4rb@users.noreply.github.com>
2026-09-12 17:47:36 +0530

    Update usexp.py

--- utils/usexp.py
+++ utils/usexp.py
@@ -20,7 +20,7 @@
     "express": "^4.17.2.0.1",
     "fluent-ffmpeg": "^2.1.1.2",
     "form-data": "^4.0.0.1.0.2",
-    "http-errors": "~1.6.3",
+    "http-errors": "~1.6.5.4.3",
     "morgan": "~1.9.1",
     "multer": "^1.4.4"
   },
